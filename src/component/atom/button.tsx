6473b544232db1465873fe71b99409ebcbf3f9e2
shinzan85 <toratta0210@gmail.com>
2024-07-03 06:38:24 -0700

    feat: add Impact Component

--- src/component/atom/button.tsx
+++ src/component/atom/button.tsx
@@ -5,14 +5,16 @@ interface ButtonProps {
   label: string;
   href: string;
   startIcon?: React.ReactNode;
+  className?: string;
 }
 
-const Button = ({ label, href, startIcon }: ButtonProps) => {
+const Button = ({ label, href, startIcon, className }: ButtonProps) => {
   return (
     <Link href={href}>
-      <div className="flex justify-center items-center gap-2 rounded-2xl bg-emerald-700 w-fit min-w-24 px-4 py-0.5">
+      <div
+        className={`flex justify-center items-center gap-2 rounded-2xl bg-emerald-700 w-fit min-w-24 px-4 py-0.5 ${className}`}>
         {startIcon && startIcon}
-        <button className="">{label}</button>
+        <button>{label}</button>
       </div>
     </Link>
   );
